6f22b772a15a8b32559b50eed8e910788cbc0afb
Satyam Singh <saty5685@gmail.com>
2021-12-19 08:05:06 +0530

    Schema Created and basic testing

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,13 +1,44 @@
 const express=require('express')
 const router=express.Router()
+const Subscriber=require('../models/Subscribers')
 //getting all subscribers
-router.get('/',(req,res)=>{
-    res.send("Hello satyam ")
+router.get('/',async (req,res)=>{
+    try{
+    const subscribers=await Subscriber.find()
+    res.send(subscribers)
+
+    }
+    catch(err){
+   res.status(500).json({message:err.message})
+    }
 })
 //getting One subscriber
+router.get('/:id',async (req,res)=>{
+    try{
+    const subscriber=await Subscriber.findById(req.params.id)
+    res.send(subscriber)
 
+    }
+    catch(err){
+   res.status(400).json(err)
+    }
+})
 //Creating a  subscriber
-
+router.post('/', async (req,res)=>{
+    const nsb=new Subscriber({
+        name :req.body.name,
+        subscribedToChannel : req.body.subscribedToChannel
+    })
+    try{
+        const newnsb=await nsb.save()
+        res.status(201).json(newnsb)
+    }
+    catch(err){
+     res.status(400).json(err)
+    }
+   
+    
+})
 //updating a subscriber
 //deleting a subscriber
 
